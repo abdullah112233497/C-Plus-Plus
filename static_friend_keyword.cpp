b62e4282e751c1eab13e579afb7864bf4a7800ab
Abdullah Qaisar <abdullahqaisar405@email.com>
2026-02-22 19:57:34 +0500

    Learned the friend keyword

--- static_friend_keyword.cpp
+++ static_friend_keyword.cpp
@@ -11,19 +11,33 @@ static void hello(){
     cout<<"I am a static function."<<endl;
 }
 void show();
-
+friend class B; // Friend class can access private members of the class A.
 };
+
+
+
 void A::show(){
-cout<<id<<endl;
+    cout<<id<<endl;
 }
 int A::id = 0;
 
+class B{
+public:
+void yes(A obj){
+cout<<"It is calling from the Class B: "<<obj.id<<endl;
+}
+
+
+};
 int main(){
-A c; 
-A d;
-A e;
-c.show();
-A::hello(); // static function can be called using class name without creating the object of the class.
+    A c; 
+    A d;
+    A e;
+    c.show();
+    A::hello(); // static function can be called using class name without creating the object of the class.
+    B x;
+    x.yes(c); // Friend class can access private members of the class A.
     return 0;
 }
+
 
